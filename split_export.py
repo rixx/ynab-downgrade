68d7d135dd3303e9102953fc36b94c01cbaeb09b
Tobias Kunze <r@rixx.de>
2021-11-06 11:22:36 +0100

    Deal with encoding bs

--- split_export.py
+++ split_export.py
@@ -14,9 +14,12 @@ def main():
         content = list(csv.DictReader(p))
 
     result = collections.defaultdict(list)
+    account_key = None
     for line in content:
         # Thanks for these replacements go to /u/loftyDan
         # https://www.reddit.com/r/ynab/comments/qlglh3/importing_data_from_nynab_to_ynab4/hj8dhwu/
+        if account_key is None:
+            account_key = '\ufeff"Account"' if '\ufeff"Account"' in line else "Account"
         line["Master Category"] = line.pop("Category Group").replace("Inflow", "Income")
         line["Sub Category"] = line.pop("Category").replace(
             "Ready to Assign", "Available this month"
@@ -24,7 +27,8 @@ def main():
         line["Category"] = line.pop("Category Group/Category").replace(
             "Inflow: Ready to Assign", "Income: Available this month"
         )
-        account = line.get('\ufeff"Account"') or line.get("Account")
+        account = line.pop(account_key)
+        line["Account"] = account
         result[account].append(line)
 
     for account, lines in result.items():
@@ -32,7 +36,7 @@ def main():
             writer = csv.DictWriter(
                 p,
                 fieldnames=[
-                    '\ufeff"Account"',
+                    "Account",
                     "Flag",
                     "Date",
                     "Payee",
